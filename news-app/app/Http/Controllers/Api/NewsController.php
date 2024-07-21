484275702180bbbb5e439c66a842d5b145638e9a
Krunika Sheth <91464126+krunikasheth114@users.noreply.github.com>
2024-07-22 01:44:01 +0530

    minor changes

--- news-app/app/Http/Controllers/Api/NewsController.php
+++ news-app/app/Http/Controllers/Api/NewsController.php
@@ -11,47 +11,51 @@ class NewsController extends Controller
 {
     public function fetchNews(Request $request)
     {
-        $response = Http::get("https://newsapi.org/v2/everything", [
-            'q' => 'bitcoin',
-            'apiKey' => '02f4e0f96c4543c9ae06ba7a1adbdae6',
-        ]);
-        $data = json_decode($response->getBody(), true); // Decode response to an associative array
 
-        $currentPage = $request->input('page', 1);
-        $perPage = $request->input('per_page', 5);
-        $search = $request->input('search', '');
-        $date = $request->input('publishedAt', '');
+        try {
+            $response = Http::get("https://newsapi.org/v2/everything", [
+                'q' => 'bitcoin',
+                'apiKey' => '02f4e0f96c4543c9ae06ba7a1adbdae6',
+            ]);
+            $data = json_decode($response->getBody(), true); 
+
+            $currentPage = $request->input('page', 1);
+            $perPage = $request->input('per_page', 5);
+            $search = $request->input('search', '');
+            $date = $request->input('publishedAt', '');
 
 
-        // Filter articles based on the search term
-        $filteredArticles = array_filter($data['articles'], function ($article) use ($search, $date) {
-            $matchesSearch = stripos($article['title'], $search) !== false
-                || stripos($article['description'], $search) !== false
-                || stripos($article['source']['name'], $search) !== false
-                || stripos($article['author'], $search) !== false;
+            // Filter articlesdata  
+            $filteredArticles = array_filter($data['articles'], function ($article) use ($search, $date) {
+                $matchesSearch = stripos($article['title'], $search) !== false
+                    || stripos($article['description'], $search) !== false
+                    || stripos($article['source']['name'], $search) !== false
+                    || stripos($article['author'], $search) !== false;
 
-            $matchesDate = true;
+                $matchesDate = true;
 
-            if ($date) {
-                $articleDate = new DateTime($article['publishedAt']);
-                $filterDate = DateTime::createFromFormat('m/d/Y', $date);
-                $matchesDate = $articleDate->format('Y-m-d') === $filterDate->format('Y-m-d');
-            }
+                if ($date) {
+                    $articleDate = new DateTime($article['publishedAt']);
+                    $filterDate = DateTime::createFromFormat('m/d/Y', $date);
+                    $matchesDate = $articleDate->format('Y-m-d') === $filterDate->format('Y-m-d');
+                }
 
-            return $matchesSearch && $matchesDate;
-        });
+                return $matchesSearch && $matchesDate;
+            });
 
-        // Paginate the filtered articles
-        $articles = array_slice($filteredArticles, ($currentPage - 1) * $perPage, $perPage);
+            $articles = array_slice($filteredArticles, ($currentPage - 1) * $perPage, $perPage);
 
-        $paginatedArticles = new \Illuminate\Pagination\LengthAwarePaginator(
-            $articles,
-            count($data['articles']),
-            $perPage,
-            $currentPage,
-            ['path' => url('/news')]
-        );
+            $paginatedArticles = new \Illuminate\Pagination\LengthAwarePaginator(
+                $articles,
+                count($data['articles']),
+                $perPage,
+                $currentPage,
+                ['path' => url('/news')]
+            );
 
-        return response()->json($paginatedArticles);
+            return response()->json($paginatedArticles);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 }
